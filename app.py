a26cb8f95a02ae8ba9cb45eb7f2aafb6692e3434
Shubham Nagar <74702808+ShubhamNagar010@users.noreply.github.com>
2023-03-03 19:21:41 +0000

    make some changes

--- app.py
+++ app.py
@@ -2,10 +2,39 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+
+jobs_list = [{
+  'id':1,
+  'title':'data sci',
+  'location':'delhi',
+  'salary':'10,00,000'
+}
+,{
+  'id':2,
+  'title':'frontend engineer',
+  'location':'pune',
+  'salary':'11,00,000'
+}
+,{
+  'id':3,
+  'title':'backend engineer',
+  'location':'banglore',
+  # 'salary':'15,00,000'
+},
+{
+  'id':4,
+  'title':'data analyst',
+  'location':'Mumbai',
+  'salary':'12,00,000'
+}
+      
+      ]
+
+
 @app.route("/")
 
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=jobs_list)
 
 
 
